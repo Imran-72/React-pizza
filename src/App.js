989f19ac77c80f489a975a10e08dc90be09c3a53
Imran Abdurakhmanov <petrow318@gmail.com>
2021-06-18 22:46:34 +0300

    fifth commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from "react";
-import {Header} from "./Components";
+import {Header, PizzaBlock} from "./Components";
 import {Cart, Home} from "./pages";
 import {Route} from "react-router-dom"
+import axios from "axios"
 
 function App() {
+    const [pizzas, setPizzas] = React.useState([])
+
+    React.useEffect(() => {
+        axios.get('http://localhost:3000/db.json').then(({data}) => {
+            setPizzas(data.pizzas)
+        })
+    }, [])
     return (
         <div className="wrapper">
             <Header/>
             <div className="content">
-                <Route path='/' component={Home} exact/>
+                <Route path='/home' render={() => <Home items={pizzas}/>} exact/>
                 <Route path='/cart' component={Cart} exact/>
-                <Home/>
             </div>
         </div>
     );
 }
+
 
 export default App;
