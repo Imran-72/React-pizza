019dcb0f411699f057489d3f28a2d282094cb05e
Imran Abdurakhmanov <petrow318@gmail.com>
2021-06-20 17:45:34 +0300

    8.1 commit

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -6,8 +6,7 @@ export const setLoaded = (payload) => ({
 })
 export const fetchPizzas = (sortBy, category) => (dispatch) => {
     dispatch(setLoaded(false))
-    axios.get(`http://localhost:3001/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${sortBy.type}
-    &_order=${sortBy.order}`).then(({data}) => {
+    axios.get(`http://localhost:3001/pizzas?${category !== null ? `category=${category}` : ''}&_sort=${sortBy.type}&_order=${sortBy.order}`).then(({data}) => {
         dispatch(setPizzas(data))
     })
 }
